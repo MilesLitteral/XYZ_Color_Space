0adefe8f23a7978d6d7e8a38ec41dea84f8b26ad
SasoriZero <Mandaloe2@gmail.com>
2022-02-24 11:54:15 -0800

    First Upload

---
+++ XYZ_Test.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+import           Control.Arrow
+import           Control.Concurrent
+import           Control.Monad.Random
+import           Control.Monad.Reader
+import           Data.Colour.RGBSpace
+import           Data.Colour.RGBSpace.HSV
+import           Data.Foldable            (for_)
+import           Data.List                (nub)
+import           Data.Semigroup           ((<>))
+import           Data.Time.Clock.POSIX
+import           Graphics.Rendering.Cairo
+import           Linear.V2
+import           Linear.Vector
+import qualified Numeric.Noise.Perlin     as P
+import           Text.Printf
+import           Linear.V3
+
+findX :: (Integral b, Fractional (b -> a), Fractional (a -> b -> a), Num a, Ord (b -> a)) => (b -> a) -> a -> b -> a
+findX x = 
+    if (x > 0.008856) 
+    then return pow(x, 1 / 3.0) 
+    else return (7.787 * x) + (16.0 / 116.0)
+
+findY :: (Integral b, Fractional (b -> a), Fractional (a -> b -> a), Num a, Ord (b -> a)) => (b -> a) -> a -> b -> a
+findY y = 
+    if (y > 0.008856)
+    then return pow(y, 1 / 3.0)
+    else return (7.787 * y) + (16.0 / 116.0)
+
+findZ :: (Integral b, Fractional (b -> a), Fractional (a -> b -> a), Num a, Ord (b -> a)) => (b -> a) -> a -> b -> a
+findZ z = 
+    if (z > 0.008856)
+    then return pow(z, 1 / 3.0)
+    else return (7.787 * z) + (16.0 / 116.0)
+
+pow :: (Num a, Integral b) => a -> b -> a
+pow value expo = value ^ expo
+
+dotprod r g b = (r + g + b) - 128
+
+xyzColor cx cy cz = do 
+    let l = 116 * findY (cx / 95.047) - 16
+    let a = 500 * findX (cx / 95.047) - findY (cy / 100.0)
+    let b = 200 * findY (cy / 100.0) -  findZ (cz / 108.883)
+    return V3 (l, a, b)
+
+getSize :: Num a => Generate (a, a)
+getSize = do
+  (w, h) <- asks (worldWidth &&& worldHeight)
+  pure (fromIntegral w, fromIntegral h)
+
+fillScreen :: (Double -> Render a) -> Double -> Generate ()
+fillScreen color opacity = do
+  (w, h) <- getSize @Double
+  cairo $ do
+    rectangle 0 0 w h
+    color opacity *> fill
+
+teaGreen :: Double -> Render ()
+teaGreen = hsva 81 0.25 0.94
+
+vividTangerine :: Double -> Render ()
+vividTangerine = hsva 11 0.40 0.92
+
+englishVermillion :: Double -> Render ()
+englishVermillion = hsva 355 0.68 0.84
+
+darkGunmetal :: Double -> Render ()
+darkGunmetal = hsva 170 0.30 0.16
+
+renderSketch :: Generate ()
+renderSketch = do
+  fillScreen eggshell 1
+
+  cairo $ setLineWidth 0.15
+
+  quads <- genQuadGrid
+  noisyQuads <- traverse quadAddNoise quads
+
+  for_ noisyQuads $ \quad -> do
+    strokeOrFill <- weighted [(fill, 0.4), (stroke, 0.6)]
+    color <- uniform
+       [ teaGreen
+       , vividTangerine
+       , englishVermillion
+       , darkGunmetal
+       ]
+    cairo $ do
+      renderQuad quad
+      color 1 *> strokeOrFill
+
+main :: IO()
+main = do
+    let xyz = xyzColor $ V3 (204 105 255)
+    let normalVal = dotprod $ xyz ^. _1 xyz ^. _2 xyz ^. _3
+    print $ show xyz
+    print normalVal
+
---
+++ XYZ_Test.py
@@ -0,0 +1,167 @@
+import math
+import random
+from PIL import Image
+from collections import namedtuple
+from enum import Enum, auto
+
+
+XYZ = namedtuple('XYZ', 'x y z')
+CIE = namedtuple('CIE', "l a b")
+RGB = namedtuple('RGB', "r g b")
+
+#Create ACTG Like Gene Graphics
+class GeneRender:
+    
+    #ACTG CONSTANTS
+    class Color(Enum):
+        RED = auto()
+        BLUE = auto()
+        GREEN = auto()
+        YELLOW = auto()
+    #EXIT ACTG CONSTANTS
+
+    def __init__(self, x=500, y=600):
+        self.x = x
+        self.y = y
+        self.img = Image.new("RGB",(x, y), "black")
+        self.NORENDER = Image.new("RGB",(x, 50), "white")
+
+        #img = Image.new("RGB",(800,600), "black")
+        # self.lay1 = Image.new("RGB",(50, 50), "white") #Template for Bar
+        # self.lay2 = Image.new("RGB",(50, 50), "red")
+        # self.lay3 = Image.new("RGB",(50, 50), "blue")
+        # self.lay4 = Image.new("RGB",(50, 50), "green")
+        # self.lay5 = Image.new("RGB",(50, 50), "yellow")
+        
+    def initialRender(self, rowCount):
+        #background = img
+        rowVar = 80
+        for x in rowCount:
+            self.img.paste(self.noRender, (10, rowVar))
+            rowVar = rowVar + 80
+
+    def showRender(self):
+        self.img.show()
+
+    def finishRender(self) -> Image:
+        return self.img
+        
+    def renderData(self, data, show: bool):
+        self.img = Image.new("RGB",(self.x, self.y), data)
+        self.finishRender()
+        if show != True:
+            return 
+        else:
+            self.showRender()
+
+def dotprod(r, g, b):
+    return (r + g + b) - 128
+
+def getColorDeltaE(c1, c2):
+    xyzC1 = rgbToXyz(c1)
+    xyzC2 = rgbToXyz(c2)
+    labC1 = xyzToCIELAB(xyzC1)
+    labC2 = xyzToCIELAB(xyzC2)
+    
+    #Euclidian Distance between two points in 3D matrices
+    deltaE = math.sqrt( pow(labC1.l - labC2.l, 2) + pow(labC1.a - labC2.a, 2) + pow(labC1.b - labC2.b, 2) )
+    return deltaE
+
+
+def rgbToXyz(c, radian):
+    #See Also
+    #https://www.falloutsoftware.com/tutorials/gl/normal-map.html
+    
+    r = c.r / 255.0; 
+    g = c.g / 255.0; 
+    b = c.b / 255.0
+    
+    if (r > 0.04045):
+        r = pow(( (r + 0.055) / 1.055 ), 2.4)
+    else:
+        r /= 12.92
+    
+    if (g > 0.04045):
+        g = pow(( (g + 0.055) / 1.055 ), 2.4)
+    else:
+        g /= 12.92
+    
+    if (b > 0.04045):
+        b = pow(( (b + 0.055) / 1.055 ), 2.4)
+    else:
+        b /= 12.92
+    
+    r *= 100 
+    g *= 100 
+    b *= 100
+
+    #Calibration for observer @2° with illumination = D65
+    #0.0f - 1.0f
+    #radian[0].x = 0.4124
+    #radian[0].y = 0.3576
+    #radian[0].z = 0.1805
+
+    #radian[1].x =  0.2126 
+    #radian[1].y =  0.7152
+    #radian[1].z =  0.0722
+    
+    #radian[2].z =  0.0193
+    #radian[2].z =  0.1192
+    #radian[2].z =  0.9505
+    
+    x = r * radian[0].x + g * radian[0].y + b * radian[0].z
+    y = r * radian[1].x + g * radian[1].y + b * radian[1].z
+    z = r * radian[2].x + g * radian[2].y + b * radian[2].z
+    
+    print(f"Frag Stride: {1.0 / 255}")
+    return XYZ(math.ceil(x), math.ceil(y), math.ceil(z))
+
+
+def xyzToCIELAB(c):
+    refX = 95.047 
+    refY = 100.0 
+    refZ = 108.883
+    
+    #References set at calibration for observer @2° with illumination = D65
+    x = c.x / refX; y = c.y / refY; z = c.z / refZ
+    
+    if (x > 0.008856):
+        x = pow(x, 1 / 3.0)
+    else: 
+        x = (7.787 * x) + (16.0 / 116.0)
+    
+    if (y > 0.008856):
+        y = pow(y, 1 / 3.0)
+    else:
+        y = (7.787 * y) + (16.0 / 116.0)
+    
+    if (z > 0.008856):
+        z = pow(z, 1 / 3.0)
+    else:
+        z = (7.787 * z) + (16.0 / 116.0)
+    
+    l = 116 * y - 16
+    a = 500 * (x - y)
+    b = 200 * (y - z)
+    
+    return CIE(math.ceil(l), math.ceil(a), math.ceil(b))
+
+def main():
+    radian = []
+
+    for x in range(0, 3):
+        radian.append(XYZ(random.random(), 1, 1))
+
+    for x in range(0, 2):
+        r = RGB(255, 0, 0)
+        print(f"RGB Color Value {r}")
+        xyz = rgbToXyz(r, radian) #xyzColor(100, 0, 255)
+        normalVal = dotprod(xyz.x, xyz.y, xyz.z)
+        print(f"XYZ Space Value {xyz}")
+        print(f"Tangent Value: {abs(normalVal)}")
+        print(f"CIE Color Value: {xyzToCIELAB(xyz)}")
+
+        Renderer = GeneRender()
+        Renderer.renderData(xyz, True)
+
+main()
